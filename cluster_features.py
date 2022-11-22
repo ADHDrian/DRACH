69d3b819af621393abb65010e8ce0a70d8e0e071
Adrian Chan <adrian.kaho.chan@gmail.com>
2022-11-22 16:40:40 +0100

    refactor parse_miclip_sites

--- cluster_features.py
+++ cluster_features.py
@@ -1,3 +1,4 @@
+import os
 import pickle
 from Bio.pairwise2 import align, format_alignment
 import numpy as np
@@ -23,12 +24,15 @@ def calc_mod_pos(in_str, burner=ref[:(span+1)]):
 
 
 file = '/home/adrian/Data/DRACH/TAATCCCAGCACTTTGGGAGG/loss_pred_label_feature.pkl'
+img_out = '/home/adrian/img_out/DRACH'
+os.makedirs(img_out, exist_ok=True)
 
 vocab = { 1:"A", 2:"C", 3:"G", 4:"T" }
 
 with open(file, 'rb') as f:
     loss_pred_label_feature = pickle.load(f)
 
+motifs = []
 mat_feature = []
 for event in loss_pred_label_feature.values():
     loss = event['loss']
@@ -44,12 +48,14 @@ for event in loss_pred_label_feature.values():
 
         # print(format_alignment(*aligned))
         # print(aligned.seqB[pred_mod_pos-1:pred_mod_pos+2])
-        print(aligned.seqA[:ref_mod_pos+1])
-        print(pred_label[:pred_mod_pos+1])
-        print('\n')
+        # print(aligned.seqA[:ref_mod_pos+1])
+        # print(pred_label[:pred_mod_pos+1])
+        # print('\n')
 
         mod_feature = feature[pred_mod_pos]
         mat_feature.append(mod_feature)
+
+        motifs.append(pred_label[(pred_mod_pos-2):(pred_mod_pos+3)])
 
         # plt.figure()
         # plt.plot(mod_feature)
@@ -63,18 +69,62 @@ mat_feature = np.vstack(mat_feature)
 from scipy.spatial.distance import pdist
 c_vec = 1.0 - pdist(mat_feature, 'cosine')
 
+c_thresh = 0.20
+
 plt.figure(figsize=(10, 6))
 plt.hist(c_vec, bins=50, range=[-1, 1])
 plt.xlabel('Cosine similarity')
 plt.ylabel('Counts')
+plt.axvline(c_thresh, color='r')
 plt.title('{}\n{} samples'.format(ref, len(mat_feature)))
-plt.show()
+plt.savefig(os.path.join(img_out, 'hist_similarity.png'), bbox_inches='tight')
+# plt.show()
+
+### connected components ###
+# from scipy.sparse import coo_matrix
+# from scipy.sparse.csgraph import connected_components
+#
+# i_vec, j_vec = np.triu_indices(len(mat_feature), k=1)
+# # c_mat = coo_matrix((c_vec, (i_vec, j_vec)), shape=(len(mat_feature), len(mat_feature)))
+# #
+# # plt.figure(figsize=(8, 8))
+# # plt.imshow(c_mat.toarray())
+# # plt.show()
+#
+# thresh_ncpts_G = {}
+# for c_thresh in np.arange(0, 1, 0.01):
+#     c_mask = c_vec >= c_thresh
+#     sel_i_vec = i_vec[c_mask]
+#     sel_j_vec = j_vec[c_mask]
+#     sel_c_vec = c_vec[c_mask]
+#     c_mat = coo_matrix((sel_c_vec, (sel_i_vec, sel_j_vec)), shape=(len(mat_feature), len(mat_feature)))
+#     n_cpts, membership = connected_components(c_mat, directed=False)
+#     G = Counter(membership).most_common(1)[0][1]
+#     thresh_ncpts_G[c_thresh] = (n_cpts, G)
+#
+# plt.figure(figsize=(10, 6))
+# plt.plot(thresh_ncpts_G.keys(), [val[0] for val in thresh_ncpts_G.values()], label='N')
+# plt.plot(thresh_ncpts_G.keys(), [val[1] for val in thresh_ncpts_G.values()], label='G')
+# plt.xlabel('C threshold')
+# # plt.ylabel('Connected components')
+# plt.legend(loc='upper left')
+# plt.savefig('/home/adrian/Data/DRACH/TAATCCCAGCACTTTGGGAGG/thresh_conn_cpts.png', bbox_inches='tight')
+# plt.show()
+
+### fix c thresh ###
+# c_thresh = 0.80
+# c_mask = c_vec >= c_thresh
+# sel_i_vec = i_vec[c_mask]
+# sel_j_vec = j_vec[c_mask]
+# sel_c_vec = c_vec[c_mask]
+# c_mat = coo_matrix((sel_c_vec, (sel_i_vec, sel_j_vec)), shape=(len(mat_feature), len(mat_feature)))
+# n_cpts, membership = connected_components(c_mat, directed=False)
+
 
 ### construct graph ###
 import igraph as ig
 import leidenalg as la
 
-c_thresh = 0.25
 i_vec, j_vec = np.triu_indices(len(mat_feature), k=1)
 c_mask = c_vec>=c_thresh
 
@@ -86,9 +136,42 @@ g.add_vertices(len(mat_feature))
 g.add_edges(zip(sel_i_vec, sel_j_vec))
 g.es['weight'] = sel_c_vec
 partition = la.find_partition(g, la.ModularityVertexPartition, weights='weight')
+membership = np.array(partition.membership)
+modularity = partition.modularity
 
+num_clusters = len(np.unique(membership))
+palette = ig.RainbowPalette(n=num_clusters)
+for cluster_ind in range(num_clusters):
+    event_ind = np.where(membership==cluster_ind)[0]
+    g.vs[event_ind]["color"] = cluster_ind
+    cluster_edges = g.es.select(_within=g.vs[event_ind])
+    cluster_edges["color"] = cluster_ind
+
+fig = plt.figure(figsize=(10, 10))
+ax = fig.add_subplot(111)
 ig.plot(
     g,
-    layout=g.layout_auto(),
-    target='/home/adrian/Data/DRACH/TAATCCCAGCACTTTGGGAGG/cluster.pdf'
+    layout = g.layout_fruchterman_reingold(),
+    bbox = (100, 100),
+    palette=palette,
+    edge_width=1,
+    target=ax,
+    vertex_size=0.1,
 )
+plt.title('Modularity {:.2f}'.format(modularity))
+plt.savefig(os.path.join(img_out, 'clusters.png'), bbox_inches='tight')
+
+### outlier ###
+counts = Counter(membership).most_common()
+# outlier_indices = np.where(membership==counts[-1][0])[0]
+# outlier_motifs = [motifs[i] for i in outlier_indices]
+# inlier_indices = [i for i in range(len(motifs)) if i not in outlier_indices]
+# inlier_motifs = [motifs[i] for i in inlier_indices]
+
+print('Thresh {}:'.format(c_thresh))
+for cluster_ind, cluster_size in counts:
+    print('Cluster {}, {} reads'.format(cluster_ind, cluster_size))
+    indices = np.where(membership==cluster_ind)[0]
+    index_motifs = np.array(motifs)[indices]
+    for k, v in Counter(index_motifs).most_common():
+        print('{}: {}'.format(k[::-1], v))
--- parse_miclip_sites.py
+++ parse_miclip_sites.py
@@ -8,68 +8,102 @@ from Bio import SeqIO, Align
 
 from collections import Counter
 
-miclip_file = '/home/achan/Data/DRACH/miCLIP_union_flat_exclude_Y_chromosome.bed'
-genome_file = '/home/achan/Data/genomes/GRCh38_96.fa'
-outdir = '/home/achan/Data/DRACH'
+# BARCODE_SPAN = 8
+# WORKSPACE = '/prj/Isabel_IVT_Nanopore/HEK293A_wildtype/Jessica_HEK293/HEK293A_2/20190409_1503_GA10000_FAK10978_2e75d7be/achan/taiyaki'
+# MOTIF = 'GGACT'
+# miclip_file = '/home/achan/Data/DRACH/miCLIP_union_flat_exclude_Y_chromosome.bed'
+# genome_ref = '/home/achan/Data/genomes/GRCh38_96.fa'
+# genome_bam_file = os.path.join(WORKSPACE, 'aligned_genome.bam.sorted')
+# transcriptome_bam_file = os.path.join(WORKSPACE, 'aligned_transcriptome.bam.sorted')
+# out_transcriptome_bam_file = os.path.join(WORKSPACE, 'transcriptome_{}.bam'.format(MOTIF))
 
-miclip = pd.read_csv(miclip_file, sep='\t', names=['chromosome', 'start_pos', 'end_pos', 'authors', 'score', 'strand'])
-mask_chrom = (pd.to_numeric(miclip['chromosome'], errors='coerce').notnull() | miclip['chromosome'].isin(['X', 'Y']))
-miclip = miclip[mask_chrom]
-miclip = miclip.reset_index(drop=True)
+def parse_genome_ref(genome_ref):
+    genome = {}
+    with open(genome_ref, 'r') as handle:
+        for record in SeqIO.parse(handle, format='fasta'):
+            id = record.id
+            if id.isnumeric() or id in ['X', 'Y']:
+                print(id)
+                genome[id] = record.seq
+    return genome
 
-genome = {}
-with open(genome_file, 'r') as handle:
-    for record in SeqIO.parse(handle, format='fasta'):
-        id = record.id
-        if id.isnumeric() or id in ['X', 'Y']:
-            print(id)
-            genome[id] = record.seq
+def parse_miclip_file(miclip_file, genome, motif):
+    miclip = pd.read_csv(miclip_file, sep='\t', names=['chromosome', 'start_pos', 'end_pos', 'authors', 'score', 'strand'])
+    mask_chrom = (pd.to_numeric(miclip['chromosome'], errors='coerce').notnull() | miclip['chromosome'].isin(['X', 'Y']))
+    miclip = miclip[mask_chrom]
+    miclip = miclip.reset_index(drop=True)
 
-motifs = []
-SPAN = 10
-for ind, row in miclip.iterrows():
-    chromosome = str(row['chromosome'])
-    strand = row['strand']
-    pos = row['start_pos']
-    motif = genome[chromosome][pos-SPAN:pos+SPAN+1] if strand=='+' else genome[chromosome][pos-SPAN:pos+SPAN+1].reverse_complement()
-    print(ind, strand, motif)
-    motifs.append(str(motif))
+    barcodes = []
+    motifs = []
+    for ind, row in miclip.iterrows():
+        chromosome = str(row['chromosome'])
+        strand = row['strand']
+        pos = row['start_pos']
+        barcode = genome[chromosome][pos-BARCODE_SPAN:pos+BARCODE_SPAN+1] if strand=='+' else genome[chromosome][pos-BARCODE_SPAN:pos+BARCODE_SPAN+1].reverse_complement()
+        motif = barcode[BARCODE_SPAN-2:BARCODE_SPAN+3]
+        # print(ind, strand, motif)
+        barcodes.append(str(barcode))
+        motifs.append(str(motif))
+    miclip['barcode'] = barcodes
+    miclip['motif'] = motifs
 
-miclip['motif'] = motifs
+    most_common_motifs = Counter(motifs).most_common(50)
+    for k, v in most_common_motifs:
+        print(k, v)
 
-most_common_motifs = Counter(motifs).most_common(50)
-for k, v in most_common_motifs:
-    print(k, v)
+    df_motif = miclip[miclip['motif'] == motif]
+    df_motif.reset_index(drop=True, inplace=True)
+    df_motif.to_csv(os.path.join(WORKSPACE, 'motif_{}.bed'.format(motif)), sep='\t', header=None, index=None)
 
-### choose motif ###
-this_motif = most_common_motifs[0][0]
-df_motif = miclip[miclip['motif'] == this_motif]
-df_motif['start_pos'] = df_motif['start_pos'] - SPAN
-df_motif['end_pos'] = df_motif['end_pos'] + SPAN
-df_motif.loc[:, df_motif.columns!='motif'].to_csv(os.path.join(outdir, 'motif_{}.bed'.format(this_motif)), sep='\t', header=None, index=None)
+    return df_motif
 
-### parse filtered bam file ###
-workspace = '/prj/Isabel_IVT_Nanopore/HEK293A_wildtype/Jessica_HEK293/HEK293A_2/20190409_1503_GA10000_FAK10978_2e75d7be/achan/taiyaki'
-genome_bam_file = os.path.join(workspace, 'aligned_genome.bam.sorted')
-genome_bam = pysam.AlignmentFile(genome_bam_file, 'rb')
+def parse_genome_alignment(genome_bam_file, df_motif):
+    genome_bam = pysam.AlignmentFile(genome_bam_file, 'rb')
+    site_ind_read_ids = {}
+    for site_ind, this_site in df_motif.iterrows():
+        # print(this_site)
+        this_site_fetch = genome_bam.fetch(this_site.chromosome, this_site.start_pos, this_site.end_pos)
+        read_ids = []
+        for this_read in this_site_fetch:
+            # print(str(this_read).split('\t'))
+            qname = this_read.qname
+            read_ids.append(qname)
+        site_ind_read_ids[site_ind] = read_ids
+    unique_read_ids = list(set([x for l in site_ind_read_ids.values() for x in l]))
+    return unique_read_ids
 
-transcriptome_bam_file = os.path.join(workspace, 'aligned_transcriptome.bam.sorted')
-transcriptome_bam = pysam.AlignmentFile(transcriptome_bam_file, 'rb')
+def write_motif_filtered_transcriptome_alignment(transcriptome_bam_file, unique_read_ids):
+    transcriptome_bam = pysam.AlignmentFile(transcriptome_bam_file, 'rb')
+    transcriptome_indexed = pysam.IndexedReads(transcriptome_bam)
+    transcriptome_indexed.build()
+    header = transcriptome_bam.header.copy()
+    out = pysam.AlignmentFile(out_transcriptome_bam_file, 'wb', header=header)
+    for this_read_id in unique_read_ids:
+        try:
+            transcriptome_indexed.find(this_read_id)
+        except KeyError:
+            pass
+        else:
+            iterator = transcriptome_indexed.find(this_read_id)
+            for x in iterator:
+                out.write(x)
+    out.close()
 
-id_seq = []
-for _, this_site in df_motif.iterrows():
-    # print(this_site)
-    this_site_iter = bam.fetch(this_site.chromosome, this_site.start_pos, this_site.end_pos)
-    for this_read in this_site_iter:
-        # print(str(this_read).split('\t'))
-        qname = this_read.qname
-        pos = this_read.pos
-        seq = this_read.seq
-        site_read_ids.append(qname)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Data.')
+    parser.add_argument("--workspace", default=None, type=str)
+    parser.add_argument("--motif", default=None, type=str)
+    parser.add_argument("--span", default=8, type=int, help="Architecture file")
+    parser.add_argument("--ref", default=None, type=str)
+    parser.add_argument("--miclip", default=None, type=str)
+    parser.add_argument("--genome_bam", default=None, type=str)
+    parser.add_argument("--transcriptome_bam", default=None, type=str)
+    parser.add_argument("--outfile", default=None, type=str)
+    args = parser.parse_args()
 
+    BARCODE_SPAN = args.span
 
-rev_motif = this_motif[::-1]
-with open(os.path.join(workspace, 'read_references_{}.fasta'.format(this_motif)), 'w') as outfile:
-    for this_read_id in site_read_ids:
-        outfile.write('>{}\n'.format(this_read_id))
-        outfile.write('{}\n'.format(rev_motif))
+    genome = parse_genome_ref(args.ref)
+    df_motif = parse_miclip_file(args.miclip, genome, args.motif)
+    read_ids = parse_genome_alignment(args.genome_bam, df_motif)
+    write_motif_filtered_transcriptome_alignment(args.transcriptome_bam, args.outfile, read_ids)
